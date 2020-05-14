0fa497f12cb285e7851dcd00f6d0836a60592f79
pwielgos <paigewielgos@gmail.com>
2020-05-13 20:30:38 -0500

    worked on bonus calculator function

--- client.js
+++ client.js
@@ -39,56 +39,42 @@ const employees = [
 
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
-function displayEmployeeBonus(employees) {
-  for (let i=0; i<employees.length; i++ )
-  console.log(calculateBonus(employees[i]));
-  
-}
 
-function createNewEmployee (employee) {
-  let bonusPercentage = getBonusPercentage(employee);
-  let totalBonus = bonusPercentage * employee.annualSalary;
-  let totalCompensation = employee.annualSalary + totalBonus;
-  let bonusifiedEmployee = {
-    name: employees.name,
-    bonusPercentage: bonusPercentage,
-    totalCompensation: totalCompensation,
-    totalBonus: totalBonus
-  }
-}
-
-function getBonusPercentage(employee){
-  let bonusPercentage = 0;
-  if ( employee.reviewRating <= 2 ) {
-    return bonusPercentage;
-  }
-    else if ( employee.reviewRating === 3 ) {
-      bonusPercentage = 4;
-    }
-    else if ( employee.reviewRating === 4 ) {
-      bonusPercentage = 6;
-    }
-    else if ( employee.reviewRating === 5 ) {
-      bonusPercentage = 10;
-    }
-  if ( employee.employeeNumber.length === 4 ){
-    bonusPercentage += 5;
-  }
-  let salary = Number(employee.annualSalary)
-  if (salary > 65000){
-    bonusPercentage -= 1;
-  }
-  if (bonusPercentage > 13){
-    bonusPercentage = 13;
-  } else if ( bonusPercentage < 0){
-    bonusPercentage = 0;
-  }
-  return bonusPercentage/100;
-}
-
-console.log( createNewEmployee(employees[2]));
 console.log(employees);
 
-for (let i=0; i < employees.length; i++ ){
-  console.log(createNewEmployee(employees[i]));
-}
+function bonusCalculator(employeeToCalc) {
+  let newEmployee = {};
+    if (employeeToCalc.reviewRating <= 2) {
+      newEmployee.name = employeeToCalc.name;
+      newEmployee.bonusPercentage = 0;
+      newEmployee.totalBonus = 0;
+      newEmployee.totalCompensation = employeeToCalc.annualSalary;
+    }
+    else if (employeeToCalc.reviewRating === 3) {
+      newEmployee.name = employeeToCalc.name;
+      newEmployee.bonusPercentage = 0.04;
+      newEmployee.totalBonus = employeeToCalc.annualSalary * newEmployee.bonusPercentage;
+      newEmployee.totalCompensation = employeeToCalc.annualSalary += newEmployee.totalBonus;
+    }
+    else if (employeeToCalc.reviewRating === 4) {
+      newEmployee.name = employeeToCalc.name;
+      newEmployee.bonusPercentage = 0.06;
+      newEmployee.totalBonus = employeeToCalc.annualSalary * newEmployee.bonusPercentage;
+      newEmployee.totalCompensation = employeeToCalc.annualSalary += newEmployee.totalBonus;
+    }
+    else if (employeeToCalc.reviewRating === 5) {
+      newEmployee.name = employeeToCalc.name;
+      newEmployee.bonusPercentage = 0.1;
+      newEmployee.totalBonus = employeeToCalc.annualSalary * newEmployee.bonusPercentage;
+      newEmployee.totalCompensation = employeeToCalc.annualSalary += newEmployee.totalBonus;
+    }
+    if (employeeToCalc.employeeNumber.length === 4) {
+      newEmployee.bonusPercentage += 0.05;
+    }
+    if (employeeToCalc.annualSalary > 65000) {
+      newEmployee.bonusPercentage -= 0.01;
+    }
+    return newEmployee;    
+  }
+
+console.log(bonusCalculator((employees[0])));
